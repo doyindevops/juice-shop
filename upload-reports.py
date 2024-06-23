c9b0f7d3fdd00c5dc286d3a7f9340d6b1d2bba4d
Adedoyin Ekong <adedoyindevops@gmail.com>
2024-06-23 09:03:25 +0100

    Update upload-reports.py

--- upload-reports.py
+++ upload-reports.py
@@ -1,42 +1,50 @@
 import requests
 import sys
+import os
 
-file_name = sys.argv[1]
-scan_type = ''
+def upload_report(file_name):
+    scan_type = ''
 
-if file_name == 'gitleaks.json':
-    scan_type = 'Gitleaks Scan'
-elif file_name == 'njsscan.sarif':
-    scan_type = 'SARIF'
-elif file_name == 'semgrep.json':
-    scan_type = 'Semgrep JSON Report'
-elif file_name == 'retire.json':
-    scan_type = 'Retire.js Scan'
-elif file_name == 'trivy.json':
-    scan_type = 'Trivy Scan'
+    if file_name == 'gitleaks.json':
+        scan_type = 'Gitleaks Scan'
+    elif file_name == 'njsscan.sarif':
+        scan_type = 'SARIF'
+    elif file_name == 'semgrep.json':
+        scan_type = 'Semgrep JSON Report'
+    elif file_name == 'retire.json':
+        scan_type = 'Retire.js Scan'
+    elif file_name == 'trivy.json':
+        scan_type = 'Trivy Scan'
 
+    headers = {
+        'Authorization': f'Token {os.environ["DEFECTDOJO_API_TOKEN"]}'
+    }
 
-headers = {
-    'Authorization': 'Token ca679219925d52d40b438f197ceedfc1b9772491'
-}
+    url = 'https://demo.defectdojo.org/api/v2/import-scan/'
 
-url = 'https://demo.defectdojo.org/api/v2/import-scan/'
+    data = {
+        'active': True,
+        'verified': True,
+        'scan_type': scan_type,
+        'minimum_severity': 'Low',
+        'engagement': 4  # Replace with your actual engagement ID
+    }
 
-data = {
-    'active': True,
-    'verified': True,
-    'scan_type': scan_type,
-    'minimum_severity': 'Low',
-    'engagement': 22
-}
+    files = {
+        'file': open(file_name, 'rb')
+    }
 
-files = {
-    'file': open(file_name, 'rb')
-}
+    response = requests.post(url, headers=headers, data=data, files=files)
 
-response = requests.post(url, headers=headers, data=data, files=files)
+    if response.status_code == 201:
+        print('Scan results imported successfully')
+    else:
+        print(f'Failed to import scan results: {response.content}')
 
-if response.status_code == 201:
-    print('Scan results imported successfully')
-else:
-    print(f'Failed to import scan results: {response.content}')
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python upload-reports.py <report_file>")
+        sys.exit(1)
+
+    report_file = sys.argv[1]
+    upload_report(report_file)
